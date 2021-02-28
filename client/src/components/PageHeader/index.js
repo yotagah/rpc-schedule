9504685f032d16d6f6ecda012392e30d382526f8
Yotagah <jhon.hnng@gmail.com>
2021-02-28 15:17:03 -0300

    Limit days to variable

--- client/src/components/PageHeader/index.js
+++ client/src/components/PageHeader/index.js
@@ -5,6 +5,7 @@ import './styles.css';
 const PageHeader = (props) =>
 {
 	const [shift, setShift] = useState(0);
+	const limitDays = 5;
 
 	const months = [
 		'JANEIRO', 'FEVEREIRO', 'MARÇO', 'ABRIL', 'MAIO', 'JUNHO', 'JULHO', 'AGOSTO', 'SETEMBRO', 'OUTUBRO', 'NOVEMBRO', 'DEAEMBRO'
@@ -18,13 +19,13 @@ const PageHeader = (props) =>
 		<header>
 			<div className="image"><img src="https://s2.glbimg.com/l8vDLy5dP-mMomtA827cWso0Kl0=/46x19/i.s3.glbimg.com/v1/AUTH_b58693ed41d04a39826739159bf600a0/internal_photos/bs/2018/d/n/qBrFd5S56b1GKqlAQ9vQ/rpc.png" alt="RPC" /></div>
 			<div className="navigator">
-				{ shift > -5 &&
+				{ shift > -limitDays &&
 					<span className="btn" onClick={() => { setShift(shift-1); props.changeDate(props.previousDateURL) }}>
 						<i className="icon-arrow icon-arrow-left"></i>
 					</span>
 				}
 				{ weekDays[props.currentDate.getDay()] }, { props.currentDate.getDate() } DE { months[props.currentDate.getMonth()] }
-				{ shift < 5 &&
+				{ shift < limitDays &&
 					<span className="btn" onClick={() => { setShift(shift+1); props.changeDate(props.nextDateURL) }}>
 						<i className="icon-arrow icon-arrow-right"></i>
 					</span>
